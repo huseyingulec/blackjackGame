2caa8648a4d311aa57fa5e2b554bbd97c587008c
huseyingulec <itshuseyingulec@gmail.com>
2023-03-18 23:29:12 +0100

    created file

---
+++ index.js
@@ -0,0 +1,77 @@
+let player = {
+    name: "",
+    chips: 500
+}
+let nameInput = prompt("write a player name")
+player.name = nameInput
+
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
+function startGame() {
+    if (player.chips > 0) { 
+        isAlive === true
+        let firstCard = getRandomCard()
+        let secondCard = getRandomCard()
+        cards = [firstCard, secondCard]
+        sum = firstCard + secondCard
+        renderGame()
+    } else {showAlert()}
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+        player.chips += 200
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+        player.chips -= 100
+    }
+    messageEl.textContent = message
+    playerEl.textContent = player.name + ": $" + player.chips
+    
+}
+
+function newCard() {
+    if (sum < 21) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
+}
+
+
+function showAlert() {
+    alert ("You're out of credit, Please refresh the page to keep playing");
+}
